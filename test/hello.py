63b450b7fa0caea428ed2c75f55ce53fb62ab3ed
Your Name <your.email@example.com>
2025-10-04 08:02:36 -0400

    Add Dockerfile and configuration files

---
+++ test/hello.py
@@ -0,0 +1,12 @@
+# hello.py
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/hello")
+def hello():
+    return {"result": "Hello World"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
